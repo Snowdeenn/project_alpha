14ca6c4c95f21466994f7679b98ab6fb47c15a96
Snowdeenn <legoffelouan05@gmail.com>
2026-05-14 18:42:30 +0200

    ajout draw commande cercle

--- src/renderer/commands.rs
+++ src/renderer/commands.rs
@@ -4,4 +4,5 @@ use raylib::{color::Color};
 pub enum DrawCommand {
     Rectangle {x: i32, y: i32, w: i32, h: i32, color: Color },
     Text {text: String, x: i32, y: i32, font_size: i32, color: Color},
+    Circle {x: i32, y: i32, radius: i32, color: Color},
 }
